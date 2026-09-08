5b4fa90da6f07222c971284b9408f864887a3926
MisterSaiyan <99093418+MisterSaiyan@users.noreply.github.com>
2026-09-08 16:57:26 -0300

    Add files via upload

---
+++ scripts/FNFSkins/fnfskinsv2.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MisterSaiyan/cosas/refs/heads/main/scripts/FNFSkins/V2/AmyGFV2.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MisterSaiyan/cosas/refs/heads/main/scripts/FNFSkins/V2/SonicBFV2.lua"))()

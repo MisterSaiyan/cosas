c57de83969630298f96bc2c1a80cf2d9617a4b52
MisterSaiyan <99093418+MisterSaiyan@users.noreply.github.com>
2026-09-08 21:38:25 -0300

    Add SonicBF and AmyGF scripts to fnfskinsv1.lua

---
+++ scripts/FNFSkins/V!/fnfskinsv1.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MisterSaiyan/cosas/refs/heads/main/scripts/FNFSkins/V1/SonicBF.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MisterSaiyan/cosas/refs/heads/main/scripts/FNFSkins/V1/AmyGF.lua"))()

29e49dc79b326fd8299f6780c92ee755111179e0
MisterSaiyan <99093418+MisterSaiyan@users.noreply.github.com>
2026-09-13 14:59:25 -0300

    Add notifications for FNF Skins and head sync issues
    Added notifications for FNF Skins V2 and head sync warnings.

--- scripts/FNFSkins/fnfskinsv2.lua
+++ scripts/FNFSkins/fnfskinsv2.lua
@@ -1,3 +1,17 @@
+local CoreGui = game:GetService("CoreGui")
+        
+		game.StarterGui:SetCore("SendNotification", {
+        Title = "FNF Skins V2", 
+        Text = "Made by MisterSaiyan | Head sync comes disabled by default, enable it if you use head cosmetics", 
+        Icon = "", Duration = 10
+    })
+
+    		game.StarterGui:SetCore("SendNotification", {
+        Title = "IMPORTANT", 
+        Text = "IMPORTANT: SUPER SONIC (SUPERMOIST) STUFF IS REALLY BUGGY WITH HEAD SYNC", 
+        Icon = "", Duration = 10
+    })
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MisterSaiyan/cosas/refs/heads/main/scripts/FNFSkins/V2/AmyGFV2.lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MisterSaiyan/cosas/refs/heads/main/scripts/FNFSkins/V2/SonicBFV2.lua"))()

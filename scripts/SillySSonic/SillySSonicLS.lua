f5225d1f36b3069313a4895a055327ac5f050e94
MisterSaiyan <99093418+MisterSaiyan@users.noreply.github.com>
2026-09-14 14:41:14 -0300

    Add notifications for updates and bug warnings
    Added notifications for Silly SSonic Alpha Revision 2 and important bug warning.

--- scripts/SillySSonic/SillySSonicLS.lua
+++ scripts/SillySSonic/SillySSonicLS.lua
@@ -1,1 +1,16 @@
+local CoreGui = game:GetService("CoreGui")
+        
+		game.StarterGui:SetCore("SendNotification", {
+        Title = "Silly SSonic Alpha Revision 2", 
+        Text = "Made by MisterSaiyan | Latest Update: 14-09-2026", 
+        Icon = "", Duration = 10
+    })
+
+    		game.StarterGui:SetCore("SendNotification", {
+        Title = "IMPORTANT", 
+        Text = "SUPERMOIST STUFF IS REALLY BUGGY WITH HEAD SYNC", 
+        Icon = "", Duration = 10
+    })
+
+
 loadstring(game:HttpGet("https://gist.githubusercontent.com/MisterSaiyan/c0f9e449b94b761922221d93dece54cd/raw/"))()
